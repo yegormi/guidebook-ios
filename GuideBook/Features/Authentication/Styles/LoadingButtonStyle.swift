48eecc5674582f7fe38f425e40bc7b2125bb1cd7
yegormi <egormiropoltsev79@gmail.com>
2023-11-03 00:38:29 +0200

    loading button implemented

--- GuideBook/Features/Authentication/Styles/LoadingButtonStyle.swift
+++ GuideBook/Features/Authentication/Styles/LoadingButtonStyle.swift
@@ -14,37 +14,37 @@ struct LoadingButtonStyle: View {
     
     var body: some View {
         Button(action: {
-            if !isLoading {
-                action()
-            }
+            action()
         }) {
             ZStack {
+                Text(isLoading ? "" : isSignIn ? "SIGN IN" : "SIGN UP")
+                    .font(.system(size: 14, weight: .bold, design: .default))
+                    .frame(maxWidth: .infinity, minHeight: 45)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
                 if isLoading {
                     ProgressView()
                         .progressViewStyle(CircularProgressViewStyle(tint: .primary))
                         .scaleEffect(1, anchor: .center)
-                } else {
-                    Text(isSignIn ? "SIGN IN" : "SIGN UP")
-                        .font(.system(size: 14, weight: .bold, design: .default))
-                        .frame(maxWidth: .infinity, minHeight: 45)
-                        .foregroundColor(Color.white)
-                        .background(isLoading ? Color.blue.opacity(0.3) : Color.blue)
-                        .cornerRadius(10)
                 }
+                
             }
+            .disabled(isLoading)
         }
-        .disabled(isLoading)
     }
 }
 
+
 struct LoadingButtonStyle_Previews: PreviewProvider {
     static var isSignIn: Bool = true
-    static var isLoading: Bool = true
+    static var isLoading: Bool = false
     
     static var previews: some View {
         LoadingButtonStyle(
             isSignIn: isSignIn,
             isLoading: isLoading
         ) {}
+            .previewLayout(.sizeThatFits)
     }
 }
