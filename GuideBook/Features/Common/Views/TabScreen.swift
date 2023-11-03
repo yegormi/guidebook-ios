f80e0655d9e39c7d0689b1e2c07f9970056be46a
yegormi <egormiropoltsev79@gmail.com>
2023-11-03 10:17:56 +0200

    close steps button added

--- GuideBook/Features/Common/Views/TabScreen.swift
+++ GuideBook/Features/Common/Views/TabScreen.swift
@@ -5,24 +5,12 @@ import SwiftUI
 
 struct TabScreen: View {
     @EnvironmentObject var guideVM: GuideViewModel
-    @EnvironmentObject var authVM: AuthViewModel
-    
     @State private var willShowHeader: Bool = false
-    
-    let hasNotch: Bool = UIDevice.current.hasNotch
-    
+
     var body: some View {
         VStack {
             if willShowHeader {
-                HStack {
-                    headerSection
-                        .padding(.leading, 20)
-                        .padding(.top, hasNotch ? 0 : 10)
-                    Spacer()
-                    if guideVM.isStepsPresented {
-                        dismissButton
-                    }
-                }
+                HeaderView()
             }
             TabView() {
                 HomeView()
@@ -51,25 +39,5 @@ struct TabScreen_Previews: PreviewProvider {
             .environmentObject(AuthViewModel())
             .environmentObject(FavoritesViewModel())
             .environmentObject(SettingsViewModel())
-    }
-}
-
-// MARK: - COMPONENTS
-
-extension TabScreen {
-    private var headerSection: some View {
-        Text("📘 GuideBook")
-            .font(.system(size: 20))
-            .bold()
-    }
-    
-    private var dismissButton: some View {
-        Button(action: {
-            guideVM.isStepsPresented = false
-        }) {
-            Image(systemName: "xmark")
-                .font(.system(size: 40))
-                .foregroundColor(.primary)
-        }
     }
 }
