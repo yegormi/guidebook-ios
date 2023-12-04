ddaff89ad97251c0787d69faa9e8e24636f9c8fc
Yegor Myropoltsev <72651640+yegormi@users.noreply.github.com>
2023-12-04 10:59:22 +0200

    profile cad spacing fixed

--- GuideBook/Core/Features/Settings/Views/Components/ProfileCard.swift
+++ GuideBook/Core/Features/Settings/Views/Components/ProfileCard.swift
@@ -12,7 +12,7 @@ struct ProfileCard: View {
     let user: UserInfo?
     
     var body: some View {
-        HStack(spacing: 20) {
+        HStack(spacing: 15) {
             Image(systemName: "person.circle.fill")
                 .font(.system(size: 50))
                 .frame(width: 50, height: 50)
@@ -20,20 +20,20 @@ struct ProfileCard: View {
                           size: CGSize(width: 50, height: 50), 
                           shape: .circle)
             
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 2) {
                 Text(user?.username)
                     .lineLimit(1)
                     .font(.system(size: 20))
                     .foregroundColor(.primary)
                     .skeleton(with: user == nil,
-                              shape: .rectangle)
+                              size: CGSize(width: 100, height: 20))
                 
                 Text(user?.email)
                     .lineLimit(1)
                     .font(.system(size: 16))
                     .foregroundColor(.secondary)
                     .skeleton(with: user == nil,
-                              shape: .rectangle)
+                              size: CGSize(width: 200, height: 15))
             }
         }
         .frame(height: 60)
